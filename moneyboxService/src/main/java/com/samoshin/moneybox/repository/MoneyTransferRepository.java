dad8aa24e1761be810c6affbe904e485f820982d
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-08-22 11:06:24 +0400

    change @Lock to native query

--- moneyboxService/src/main/java/com/samoshin/moneybox/repository/MoneyTransferRepository.java
+++ moneyboxService/src/main/java/com/samoshin/moneybox/repository/MoneyTransferRepository.java
@@ -4,11 +4,12 @@ import com.samoshin.moneybox.model.MoneyTransfer;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MoneyTransferRepository extends JpaRepository<MoneyTransfer, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Override
+    @Query(value = "SELECT * FROM money_transfers WHERE id = ? FOR NO KEY UPDATE", nativeQuery = true)
     Optional<MoneyTransfer> findById(Long id);
 }
--- moneyboxService/src/main/java/com/samoshin/moneybox/repository/MoneyboxRepository.java
+++ moneyboxService/src/main/java/com/samoshin/moneybox/repository/MoneyboxRepository.java
@@ -4,11 +4,12 @@ import com.samoshin.moneybox.model.Moneybox;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MoneyboxRepository extends JpaRepository<Moneybox, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Override
+    @Query(value = "SELECT mb.id,mb.sum FROM moneyboxes WHERE id=? FOR NO KEY UPDATE", nativeQuery = true)
     Optional<Moneybox> findById(Long id);
 }
